d7a65b6dc61a68374f03465665852c618b732e01
Rohit-KK15 <105338950+Rohit-KK15@users.noreply.github.com>
2023-08-21 20:47:23 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg"></div>
+  <HelloWorld />
 </template>
 
 <script>
@@ -16,11 +16,32 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+
+  font-family: 'Poppins';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* background-color: rgb(0, 0, 0, 0.7); */
 }
+
+.bg{
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  background-image: url(https://image.tmdb.org/t/p/original//wiE9doxiLwq3WCGamDIOb2PqBqc.jpg);
+  transition: all .3s;
+  background-size: cover;
+  background-position: 50% 0;
+}
+
+
+
 </style>
