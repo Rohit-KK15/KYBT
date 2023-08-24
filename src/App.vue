3d37fe9052d655bc6af27e5f7f6ecc4edf331ea0
Rohit-KK15 <105338950+Rohit-KK15@users.noreply.github.com>
2023-08-24 17:58:56 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg"></div>
+  
   <HelloWorld />
 </template>
 
@@ -16,31 +16,15 @@ export default {
 
 <style>
 #app {
-  top: 0;
-  left: 0;
   height: 100%;
   width: 100%;
-
   font-family: 'Poppins';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  /* background-color: rgb(0, 0, 0, 0.7); */
 }
 
-.bg{
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: -1;
-  width: 100%;
-  height: 100%;
-  background-image: url(https://image.tmdb.org/t/p/original//wiE9doxiLwq3WCGamDIOb2PqBqc.jpg);
-  transition: all .3s;
-  background-size: cover;
-  background-position: 50% 0;
-}
+
 
 
 
