117e26a8e51fd1d8eae396e993dc3820a4100d12
Rohit-KK15 <105338950+Rohit-KK15@users.noreply.github.com>
2023-08-25 12:50:29 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(fas, fab)
+
+loadFonts()
+
+createApp(App)
+  .component('fa', FontAwesomeIcon)
+  .mount('#app')
+
