e26453adc9bd6db6d596cedb22e65ae4fb8dca4b
Rohit-KK15 <105338950+Rohit-KK15@users.noreply.github.com>
2024-02-27 22:25:08 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,54 +1,59 @@
 <template>
-  <div class="bg"></div>
-  <div class="kybt-container">
-    <div class="heading">
-      <p style="text-shadow: 2px 2px 4px #000000;color: rgb(241, 195, 12);font-weight: bolder;font-size: 2rem;">Know Your Binge Time</p>
+  <div class="bg" :style="{ 'background-image': 'url(https://image.tmdb.org/t/p/original'+ backdropPath + ')' }"></div>
+  <div class="main-container">
+    <div class="kybt-container">
+      <div class="heading">
+        <p style="text-shadow: 2px 2px 4px #000000;color: rgb(241, 195, 12);font-weight: bolder;font-size: 2rem;">Know Your Binge Time</p>
+      </div>
+      <showtime />
+      <tvshowsearch />
     </div>
-    <tvshowsearch />
   </div>
 </template>
 
 <script>
-import tvshowsearch from './TVShowSearch.vue'
+import tvshowsearch from './TVShowSearch.vue';
+import showtime from './ShowTime.vue';
+import { getBackdropPath } from '../backdropPath.js';
+
 export default {
   components: {
     tvshowsearch,
+    showtime,
+  },
+  data() {
+    return {
+      backdropPath: null // Default backdrop path
+    };
+  },
+  created(){
+    this.changeBackdrop();
+  },
+  methods: {
+    changeBackdrop(){
+      this.backdropPath=getBackdropPath();
+    }
   }
 }
 </script>
 
 <style scoped>
 
-.kybt-container{
+.main-container{
+  z-index: 2;
   position: fixed;
+  background-color: rgb(0,0,0,0.5);
+  backdrop-filter: blur(5px);
+  width: 100%;
+  height: 100%;
+  align-items: center;
+
+}
+
+.kybt-container{
   top: 0;
   left: 0;
-  height: 100%;
-  width: 100%;
-  background-color: rgb(0,0,0,0.7);
   margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.search-container {
-    border-radius: 25px;
-    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-    display: flex;
-    overflow: hidden;
-}
-
-.search-input {
-  background-color: rgb(43, 41, 41);
-  color: white;
-  border: none;
-  padding: 10px 15px;
-  font-size: 18px;
-  width: 300px;
-  height: 25px;
-  border-radius: 25px 0 0 25px;
 }
 
 .bg{
@@ -58,10 +63,12 @@ export default {
   z-index: -1;
   width: 100%;
   height: 100%;
-  background-image: url(https://image.tmdb.org/t/p/original//wiE9doxiLwq3WCGamDIOb2PqBqc.jpg);
+  /* background-image: url("https://image.tmdb.org/t/p/original/rIe3PnM6S7IBUmvNwDkBMX0i9EZ.jpg"); */
   transition: all .3s;
   background-size: cover;
   background-position: 50% 0;
 }
+
+
 
 </style>
